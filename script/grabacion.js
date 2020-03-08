1c4c1f36896406a1cf475f6ffcda2d3a4e38585b
juanchovau <jsvargasar@poligran.edu.co>
2020-03-08 10:52:17 -0500

    aplicacion de captura casi completa

--- script/grabacion.js
+++ script/grabacion.js
@@ -1,137 +1,171 @@
-function getStreamAndRecord () { 
+let gifosAlmacen = [];
 
-    document.getElementById("captura").style.display = "none";
-    document.getElementById("captura2").style.display = "block";
-    document.getElementById("primerosBotones").style.display = "block";
-    document.getElementById("nuevosBotones").style.display = "none";
-    navigator.mediaDevices.getUserMedia({
-    
-    audio: false,
-    
-    video: {
-    
-    height: { max: 480 }
-    
-    }
-    
-    })
-  .then(function(stream) {
+function getStreamAndRecord() {
+  document.getElementById("captura").style.display = "none";
+  document.getElementById("captura2").style.display = "block";
+  document.getElementById("primerosBotones").style.display = "block";
+  document.getElementById("video").style.display = "block";
+  document.getElementById("nuevosBotones").style.display = "none";
+  document.getElementById("cajavideo").innerHTML = "";
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: false,
 
-  
-    
-    document.getElementById("video").srcObject = stream;
-    
-    document.getElementById("video").play()
-    
+      video: {
+        height: { max: 480 }
+      }
     })
+    .then(function(stream) {
+      document.getElementById("video").srcObject = stream;
+
+      document.getElementById("video").play();
+    });
 }
 
 function grbar(stream) {
-     recorder = RecordRTC(stream, {
-        type: 'video'
-    });}
+  recorder = RecordRTC(stream, {
+    type: "video"
+  });
+}
 
-    
 var image = document.getElementById("video");
 
 function captureCamera(callback) {
-    navigator.mediaDevices.getUserMedia({ video: true }).then(function(camera) {
-        callback(camera);
-    }).catch(function(error) {
-        alert('Algo salio mal');
-        console.error(error);
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then(function(camera) {
+      callback(camera);
+    })
+    .catch(function(error) {
+      alert("Algo salio mal");
+      console.error(error);
     });
 }
 
 function stopRecordingCallback() {
-    //document.querySelector('h1').innerHTML = 'Se guardo: ' + bytesToSize(recorder.getBlob().size);
-    image = URL.createObjectURL(recorder.getBlob())
-    
-    console.log(image)
-   let numeroParaAdignarLE = window.localStorage.length ;
-   let nombreParaAdignarLE =  "img" + numeroParaAdignarLE;
-   window.localStorage.setItem(nombreParaAdignarLE, image);
-    
-  
-       document.getElementById("segundosBotones").style.display = "none";
-       document.getElementById("nuevosBotones").style.display = "block";
-       let numberToGet = window.localStorage.length - 1 ;
-       let getNumber = "img" + numberToGet;
-       let myGifo = document.createElement("IMG"); 
-       myGifo.style.margin = "5px" 
-       myGifo.style.width = "290px" 
-       myGifo.style.height = "290px" 
-       myGifo.src =  localStorage.getItem(getNumber);              
-       document.getElementById("myGuifosContainer").appendChild(myGifo);
-       console.log(myGifo.src)
-   
+  //document.querySelector('h1').innerHTML = 'Se guardo: ' + bytesToSize(recorder.getBlob().size);
 
-    //recorder.camera.stop();
-    //recorder.destroy();
-    //recorder = null;
+  let img1 = URL.createObjectURL(recorder.getBlob());
+  let numeroParaAdignarLE = localStorage.length;
+  let nombreParaAdignarLE = "gif" + numeroParaAdignarLE;
+  let form = new FormData();
+  form.append(nombreParaAdignarLE, recorder.getBlob(), "myGif.gif");
+  //console.log(form.get(nombreParaAdignarLE))
+
+  document.getElementById("segundosBotones").style.display = "none";
+  document.getElementById("nuevosBotones").style.display = "block";
+  document.getElementById("video").style.display = "none";
+  let numberToGet = window.localStorage.length;
+  let getNumber = "img" + numberToGet;
+
+  let myGifo = document.createElement("IMG");
+  myGifo.src = img1;
+  myGifo.style.width = "98% ";
+  myGifo.classList.add = "gifPrev";
+  document.getElementById("cajavideo").appendChild(myGifo);
+
+  document.getElementById("subirGifo").onclick = function() {
+    this.disabled = true;
+    // fetch(
+    //   "https://upload.giphy.com/v1/gifs?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv",
+    //   {
+    //     // headers: {
+    //     //   "Content-Type": "application/gif"
+    //     // },
+    //     method: "POST",
+    //     mode: "no-cors",
+    //     referrerPolicy: 'no-referrer',
+    //      credentials: 'same-origin',
+
+    //     // body: recorder.getBlob(),
+    //     body: form.get(nombreParaAdignarLE)
+    //   }
+    // )
+    //   //.then(response => response) //.json())
+    //   .then(data => {
+    //     console.log("Success:", data);
+    //     //document.getElementById("hola").src = data.url
+    //   })
+    //   .catch(error => {
+    //     console.error("MyError:", error);
+    //   });
+    let myGifos = document.createElement("IMG");
+    myGifos.style.margin = "5px";
+    myGifos.style.width = "290px";
+    myGifos.style.height = "290px";
+    myGifos.src = img1;
+    document.getElementById("myGuifosContainer").appendChild(myGifos);
+    window.localStorage.setItem(getNumber, img1);
+
+    //document.querySelector("#cajavideo img").style.width = "50%"; 
+    document.getElementById("pantallafinal").style.display = "block";
+    document.getElementById("pantallafinal").style.width = "50%";
+    document.querySelector("#botonfinal").style.display = "block";
+    document.getElementById("nuevosBotones").style.display = "none";
+    let copied = document.querySelector("#cajavideo img").src
+    document.getElementById("copiargif").onclick = function(){
+      copied.execCommand('copy');
+      console.log("hola")
+    }
     
+    
+    document.getElementById("botonfinal").onclick = function() {
+      document.getElementById("captura2").style.display = "none" ; 
+      document.getElementById("captura").style.display = "block" ; 
+    }
+  
+  };
 }
 
 var recorder; // globally accessible
 
-
-
-document.getElementById('primerosBotones').onclick = function() {
-    this.disabled = true;
-    captureCamera(function(camera) {
-        //document.querySelector('h1').innerHTML = 'esta por empezar';
-        recorder = RecordRTC(camera, {
-            type: 'gif',
-            frameRate: 1,
-            quality: 10,
-            width: 360,
-            hidden: 240,
-            onGifRecordingStarted: function() {
-                document.getElementById("primerosBotones").style.display = "none";
-                document.getElementById("nuevosBotones").style.display = "none";
-                document.getElementById("segundosBotones").style.display = "block";
-            },
-            onGifPreview: function(gifURL) {
-               
-               // document.getElementById("video") .src = gifURL;
-                
-            }
-        });
-
-        recorder.startRecording();
-
-        // release camera on stopRecording
-        recorder.camera = camera;
-
-        document.getElementById('primerosBotones').disabled = false;
- 
+document.getElementById("primerosBotones").onclick = function() {
+  this.disabled = true;
+  captureCamera(function(camera) {
+    //document.querySelector('h1').innerHTML = 'esta por empezar';
+    recorder = RecordRTC(camera, {
+      type: "gif",
+      frameRate: 1,
+      quality: 10,
+      width: 360,
+      hidden: 240,
+      onGifRecordingStarted: function() {
+        document.getElementById("primerosBotones").style.display = "none";
+        document.getElementById("nuevosBotones").style.display = "none";
+        document.getElementById("segundosBotones").style.display = "block";
+      },
+      onGifPreview: function(gifURL) {
+        // document.getElementById("video") .src = gifURL;
+      }
     });
+
+    recorder.startRecording();
+
+    // release camera on stopRecording
+    recorder.camera = camera;
+
+    document.getElementById("primerosBotones").disabled = false;
+  });
 };
 
-document.getElementById("segundosBotones").onclick = function(){
-    this.disabled = true;
-    recorder.stopRecording(stopRecordingCallback);
- 
-    
+document.getElementById("segundosBotones").onclick = function() {
+  this.disabled = true;
+  recorder.stopRecording(stopRecordingCallback);
 };
-
-
 
 //convocar local storage
 
-
-if(localStorage.length > 0){
-
- for(let i = 0; i < localStorage.length; i++ ){
-
-    let numberToGet = i ;
+if (localStorage.length > 0) {
+  for (let i = 0; i < localStorage.length; i++) {
+    let numberToGet = i;
     let getNumber = "img" + numberToGet;
-    let myGifo = document.createElement("IMG"); 
-    myGifo.style.margin = "5px" 
-    myGifo.style.width = "290px" 
-    myGifo.style.height = "290px" 
-    myGifo.src =  localStorage.getItem(getNumber);               
+    let myGifo = document.createElement("IMG");
+    myGifo.style.margin = "5px";
+    myGifo.style.width = "290px";
+    myGifo.style.height = "290px";
+    myGifo.src = localStorage.getItem(getNumber);
+
+    console.log(myGifo.src);
     document.getElementById("myGuifosContainer").appendChild(myGifo);
-    console.log(myGifo.src)
- }
+  }
 }
--- script/myscripts.js
+++ script/myscripts.js
@@ -1,187 +1,194 @@
 //Inicio función cambio de tema
 
-
 let subboton1 = document.getElementById("subboton1");
-let subboton2 = document.getElementById("subboton2")
-function dark(){
-    document.getElementById("ligthlogo").className = "erase";
-    document.getElementById("darklogo").className = "show";
-    document.getElementById("body").className = "darkmode";
-    document.getElementById("subboton1").className = "botonclaro";
-    document.getElementById("subboton2").className = "botonclaro";
-    document.getElementById("botonflecha").className = "botonclaro";
-    document.getElementById("botoncrear").className = "botonclaro";
-    document.getElementById("boton1").className = "botonclaro";
-    document.getElementById("misguifos").style.color = "white";
-    subboton1.style.display = "none";
-    subboton2.style.display = "none";
+let subboton2 = document.getElementById("subboton2");
+function dark() {
+  document.getElementById("ligthlogo").className = "erase";
+  document.getElementById("darklogo").className = "show";
+  document.getElementById("body").className = "darkmode";
+  document.getElementById("subboton1").className = "botonclaro";
+  document.getElementById("subboton2").className = "botonclaro";
+  document.getElementById("botonflecha").className = "botonclaro";
+  document.getElementById("botoncrear").className = "botonclaro";
+  document.getElementById("boton1").className = "botonclaro";
+  document.getElementById("misguifos").style.color = "white";
+  subboton1.style.display = "none";
+  subboton2.style.display = "none";
 }
 
-function ligth(){
-    document.getElementById("ligthlogo").className = "show";
-    document.getElementById("darklogo").className = "erase";
-    document.getElementById("body").className = "ligthmode";
-    document.getElementById("subboton1").className = "botonrosa";
-    document.getElementById("subboton2").className = "botonrosa";
-    document.getElementById("botonflecha").className = "botonrosa";
-    document.getElementById("botoncrear").className = "botonrosa";
-    document.getElementById("boton1").className = "botonrosa";
-    subboton1.style.display = "none";
-    subboton2.style.display = "none";
-    document.getElementById("misguifos").style.color = "#110038";
+function ligth() {
+  document.getElementById("ligthlogo").className = "show";
+  document.getElementById("darklogo").className = "erase";
+  document.getElementById("body").className = "ligthmode";
+  document.getElementById("subboton1").className = "botonrosa";
+  document.getElementById("subboton2").className = "botonrosa";
+  document.getElementById("botonflecha").className = "botonrosa";
+  document.getElementById("botoncrear").className = "botonrosa";
+  document.getElementById("boton1").className = "botonrosa";
+  subboton1.style.display = "none";
+  subboton2.style.display = "none";
+  document.getElementById("misguifos").style.color = "#110038";
 }
 
-function mostrarSubMenu(){
-   
-    subboton1.style.display = "block";
-    subboton2.style.display = "block";
-
+function mostrarSubMenu() {
+  subboton1.style.display = "block";
+  subboton2.style.display = "block";
 }
 //Final función cambio de tema
 
 //Inicio barra de busqueda
 
-function buscarGifs(){
+function buscarGifs() {
+  let criterioDeBusqueda = document.getElementById("criterioDeBusqueda").value;
 
-let criterioDeBusqueda = document.getElementById("criterioDeBusqueda").value;
-
-function establecerCriterio (criterio){
-    for (let i = 0; i < 24; i++){
-    
-        fetch("https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=" + criterio + "&limit=25&offset=0&rating=G&lang=en")
-        .then( (response) => { 
-            return response.json();
+  function establecerCriterio(criterio) {
+    for (let i = 0; i < 24; i++) {
+      fetch(
+        "https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=" +
+          criterio +
+          "&limit=25&offset=0&rating=G&lang=en"
+      )
+        .then(response => {
+          return response.json();
         })
-        .then( (json) => {
-    
-            return json.data[i].images.downsized.url
-    
+        .then(json => {
+          return json.data[i].images.downsized.url;
         })
-        .then((data1) => {
-          
+        .then(data1 => {
+          let hijosDeBusqueda = document.getElementById("lienzoBusqueda")
+            .childNodes;
 
-            
-            let hijosDeBusqueda = document.getElementById("lienzoBusqueda").childNodes;  
-            
-            if(hijosDeBusqueda.length > 23){
+          if (hijosDeBusqueda.length > 23) {
+            hijosDeBusqueda[i].src = data1;
+          } else {
+            var img1 = document.createElement("IMG");
+            img1.style.margin = "5px";
+            img1.style.width = "290px";
+            img1.style.height = "290px";
+            img1.src = data1;
+            document.getElementById("lienzoBusqueda").appendChild(img1);
+          }
+        });
+    }
+  }
 
-                hijosDeBusqueda[i].src = data1;
-            }else{
-                var img1 = document.createElement("IMG"); 
-                img1.style.margin = "5px" 
-                img1.style.width = "290px" 
-                img1.style.height = "290px" 
-                img1.src = data1  
-                document.getElementById("lienzoBusqueda").appendChild(img1); 
-
-            }
-            
-    
-        })
-     }
-}
-
-establecerCriterio(criterioDeBusqueda);
-
+  establecerCriterio(criterioDeBusqueda);
 }
 
 //Final barra de busqueda
 
 //Inicio de peticion apis para casillas
 
-fetch("https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=jonathan vanness&limit=25&offset=0&rating=G&lang=en")
-    .then( (response) => { 
-        return response.json();
-    })
-    .then( (json) => {
+fetch(
+  "https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=jonathan vanness&limit=25&offset=0&rating=G&lang=en"
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    return json.data[0].images.downsized.url;
+  })
+  .then(data1 => {
+    document.getElementById("casilla1").src = data1;
+  });
 
-        return json.data[0].images.downsized.url
-    })
-    .then((data1) => {
-       
-        document.getElementById("casilla1").src = data1  
+fetch(
+  "https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=Sailor Mercury&limit=25&offset=0&rating=G&lang=en"
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    return json.data[0].images.downsized.url;
+  })
+  .then(data1 => {
+    document.getElementById("casilla2").src = data1;
+  });
 
-    })
+fetch(
+  "https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=Fav Five&limit=25&offset=0&rating=G&lang=en"
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    return json.data[0].images.downsized.url;
+  })
+  .then(data1 => {
+    document.getElementById("casilla3").src = data1;
+  });
 
-
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=Sailor Mercury&limit=25&offset=0&rating=G&lang=en")
-    .then( (response) => { 
-        return response.json();
-    })
-    .then( (json) => {
-
-        return json.data[0].images.downsized.url
-    })
-    .then((data1) => {
-      
-        document.getElementById("casilla2").src = data1  
-
-    })
-
-
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=Fav Five&limit=25&offset=0&rating=G&lang=en")
-    .then( (response) => { 
-        return response.json();
-    })
-    .then( (json) => {
-
-        return json.data[0].images.downsized.url
-    })
-    .then((data1) => {
-        
-        document.getElementById("casilla3").src = data1  
-
-    })
-
-
-    fetch("https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=Unicors&limit=25&offset=0&rating=G&lang=en")
-    .then( (response) => { 
-        return response.json();
-    })
-    .then( (json) => {
-
-        return json.data[0].images.downsized.url
-    })
-    .then((data1) => {
-
-        document.getElementById("casilla4").src = data1  
-
-    })
-//Final de peticion apis para casillas 
+fetch(
+  "https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=Unicors&limit=25&offset=0&rating=G&lang=en"
+)
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    return json.data[0].images.downsized.url;
+  })
+  .then(data1 => {
+    document.getElementById("casilla4").src = data1;
+  });
+//Final de peticion apis para casillas
 //Inicio display tendencias
 
-
-
-for (let i = 0; i < 24; i++){
-    
-    fetch("https://api.giphy.com/v1/gifs/trending?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&limit=25&rating=G")
-    .then( (response) => { 
-        return response.json();
+for (let i = 0; i < 24; i++) {
+  fetch(
+    "https://api.giphy.com/v1/gifs/trending?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&limit=25&rating=G"
+  )
+    .then(response => {
+      return response.json();
     })
-    .then( (json) => {
-
-        return json.data[i].images.downsized.url
-
+    .then(json => {
+      return json.data[i].images.downsized.url;
     })
-    .then((data1) => {
-      
-        var img1 = document.createElement("IMG"); 
-        img1.style.margin = "5px" 
-        img1.style.width = "290px" 
-        img1.style.height = "290px" 
-        img1.src = data1                  
-        document.getElementById("tendencias").appendChild(img1);               // Append <button> to <body>
-
-    })
-
-
+    .then(data1 => {
+      var img1 = document.createElement("IMG");
+      img1.style.margin = "5px";
+      img1.style.width = "290px";
+      img1.style.height = "290px";
+      img1.src = data1;
+      document.getElementById("tendencias").appendChild(img1); // Append <button> to <body>
+    });
 }
 
-//Finaldisplay tendencias
+//Finaldisplay tendencia
+//inicio recomedaciones
 
+let recomendacionBusqueda = document.getElementById("criterioDeBusqueda");
 
+recomendacionBusqueda.addEventListener("keyup", () => {
+  let criterio = recomendacionBusqueda.value;
 
-
-
-
-
+  fetch(
+    "https://api.giphy.com/v1/gifs/search?api_key=eyWdbN1PQEckVgWNa49mXtqomVsAf8fv&q=" +
+      criterio +
+      "&limit=25&offset=0&rating=G&lang=en"
+  )
+    .then(response => {
+      return response.json();
+    })
+    .then(json => {
+      console.log(json.data[0]);
+      document.getElementById("lienzoRecomendaciones").innerHTML = "";
+      let palabras = json.data[0].title.split(" GIF");
+      return palabras[0].split(" ");
+    })
+    .then(recomendaciones => {
+      console.log(recomendaciones);
+      recomendaciones.forEach(element => {
+        var recomendacion = document.createElement("BUTTON");
+        recomendacion.style.width = "87%";
+        recomendacion.style.padding = "10px";
+        recomendacion.style.margin = "10px";
+        recomendacion.style.backgroundColor = "#E6E6E6";
+        recomendacion.style.border = "solid black 1px";
+        recomendacion.innerHTML = element;
+        recomendacion.onclick = "establecerCriterio(" + element + ")";
+        document
+          .getElementById("lienzoRecomendaciones")
+          .appendChild(recomendacion);
+      });
+    });
+});
